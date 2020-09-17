3f50a90f2a7625d358a4e60322db3bb9e5dda1a9
Ruha3h <kazimov_ruhlan@mail.ru>
2020-09-17 17:52:10 +0400

    New Logic To Play Audio

--- Pianino/Pianino/Program.cs
+++ Pianino/Pianino/Program.cs
@@ -1,7 +1,19 @@
 ﻿using System;
+using System.Threading;
+using NAudio.Wave;
 
 namespace Pianino
 {
+
+    public class Buffer : object
+    {
+        public Buffer(ConsoleKey key)
+        {
+            this.key = key;
+        }
+
+        public ConsoleKey key;
+    }
 
     static class Pianino
     {
@@ -68,33 +80,71 @@ namespace Pianino
             7902.133,
         };
 
+        public static string[] OctavesPath = new string[] {
+            "C4",
+            "C#4",
+            "D4",
+            "D#4",
+            "E4",
+            "F4",
+            "F#4",
+            "G4",
+            "G#4",
+            "A4",
+            "A#4",
+            "B4",
+            "C5",
+            "C#5",
+            "D5",
+            "D#5",
+            "E5",
+            "F5",
+            "F#5",
+            "G5",
+            "G#5",
+            "A5",
+            "A#5",
+            "B5",
+             "C6",
+            "C#6",
+            "D6",
+            "D#6",
+            "E6",
+            "F6",
+            "F#6",
+            "G6",
+            "G#6",
+            "A6",
+            "A#6",
+            "B6",
+        };
         public static int GetNote(ConsoleKey key)
         {
             switch (key)
             {
-                case ConsoleKey.Z ://Do0:
+                case ConsoleKey.Z://Do0:
                     return 0;
-                case ConsoleKey.S ://DoD0:
+                case ConsoleKey.S://DoD0:
                     return 1;
-                case ConsoleKey.X ://Re0:
+                case ConsoleKey.X://Re0:
                     return 2;
-                case ConsoleKey.D ://ReD0:
+                case ConsoleKey.D://ReD0:
                     return 3;
-                case ConsoleKey.C ://Mi0:
+                case ConsoleKey.C://Mi0:
                     return 4;
-                case ConsoleKey.V ://Fa0:
+                case ConsoleKey.V://Fa0:
                     return 5;
-                case ConsoleKey.G ://FaD0:
+                case ConsoleKey.G://FaD0:
                     return 6;
-                case ConsoleKey.B ://Sol0:
+                case ConsoleKey.B://Sol0:
                     return 7;
-                case ConsoleKey.H ://SolD0:
+                case ConsoleKey.H://SolD0:
                     return 8;
-                case ConsoleKey.N ://Lya0:
+                case ConsoleKey.N://Lya0:
                     return 9;
-                case ConsoleKey.J ://LyaD0:
+                case ConsoleKey.J://LyaD0:
                     return 10;
-                case ConsoleKey.M ://Si0:
+                case ConsoleKey.M://Si0:
                     return 11;
                 case ConsoleKey.OemComma://Do1:
                 case ConsoleKey.Q://Do1:
@@ -111,55 +161,89 @@ namespace Pianino
                 case ConsoleKey.Oem2://Mi1:
                 case ConsoleKey.E://Mi1:
                     return 16;
-                case ConsoleKey.R ://Fa1:
+                case ConsoleKey.R://Fa1:
                     return 17;
-                case ConsoleKey.D5 ://FaD1:
+                case ConsoleKey.D5://FaD1:
                     return 18;
-                case ConsoleKey.T ://Sol1:
+                case ConsoleKey.T://Sol1:
                     return 19;
-                case ConsoleKey.D6 ://SolD1:
+                case ConsoleKey.D6://SolD1:
                     return 20;
-                case ConsoleKey.Y ://Lya1:
+                case ConsoleKey.Y://Lya1:
                     return 21;
-                case ConsoleKey.D7 ://LyaD1:
+                case ConsoleKey.D7://LyaD1:
                     return 22;
-                case ConsoleKey.U ://Si1:
+                case ConsoleKey.U://Si1:
                     return 23;
-                case ConsoleKey.I ://Do2:
+                case ConsoleKey.I://Do2:
                     return 24;
-                case ConsoleKey.D9 ://DoD2:
+                case ConsoleKey.D9://DoD2:
                     return 25;
-                case ConsoleKey.O ://Re2:
+                case ConsoleKey.O://Re2:
                     return 26;
-                case ConsoleKey.D0 ://ReD2:
+                case ConsoleKey.D0://ReD2:
                     return 27;
-                case ConsoleKey.P ://Mi2:
+                case ConsoleKey.P://Mi2:
                     return 28;
-                case ConsoleKey.Oem4 ://Fa2:
+                case ConsoleKey.Oem4://Fa2:
                     return 29;
-                case ConsoleKey.OemPlus ://FaD2:
+                case ConsoleKey.OemPlus://FaD2:
                     return 30;
-                case ConsoleKey.Oem6 ://Fa2:
+                case ConsoleKey.Oem6://Fa2:
                     return 31;
             }
             return -1;
         }
 
-        public static void PlayNote(int Index)
+        public static void PlayNoteAsBeep(int Index)
         {
-            if (Index != -1) Console.Beep(Convert.ToInt32(Octaves[Index]) , 200);
+            if (Index != -1) Console.Beep(Convert.ToInt32(Octaves[Index]), 200);
+        }
+
+        public static void PlayNoteAsSound(object Index)
+        {
+            if (Convert.ToInt32(Index) != -1)
+            {
+                Console.WriteLine("Piano\\" + OctavesPath[Convert.ToInt32(Index)] + ".waw");
+                using (var audioFile = new AudioFileReader(@"C:\Piano\" + OctavesPath[Convert.ToInt32(Index)] + ".wav"))
+                {
+                    Console.WriteLine("\tStep1");
+                    using (var outputDevice = new WaveOutEvent())
+                    {
+                        Console.WriteLine("\t\tStep2");
+                        outputDevice.Init(audioFile);
+                        outputDevice.Play();
+                        while (outputDevice.PlaybackState == PlaybackState.Playing)
+                        {
+                            Console.WriteLine($"\t\t\t - {Index} - {Thread.CurrentThread.GetHashCode()} -");
+                            Thread.Sleep(5000);
+                        }
+                    }
+                }
+            }
         }
     }
 
+   
+
     class Program
     {
+        static void Action(object keyBuffer)
+        {
+            Buffer temp = keyBuffer as Buffer;
+            var NoteIndex = Pianino.GetNote(temp.key);
+
+            Pianino.PlayNoteAsSound(NoteIndex);
+        }
         static void Main(string[] args)
         {
+            //Pianino.PlayNoteAsSound(10);
+
             while (true)
             {
-                var NoteIndex = Pianino.GetNote(Console.ReadKey(true).Key);
-                Pianino.PlayNote(NoteIndex);
+                new Thread(Action).Start(new Buffer(Console.ReadKey(true).Key));
             }
         }
+        
     }
 }
