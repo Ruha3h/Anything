ae6672bae819d295b7eaed56f9d9c099e9f61b21
Ruha3h <kazimov_ruhlan@mail.ru>
2020-09-17 19:00:25 +0400

    sort by file

---
+++ Pianino/Pianino/Buffer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pianino
+{
+    public class Buffer : object
+    {
+        public Buffer(ConsoleKey key)
+        {
+            this.key = key;
+        }
+
+        public ConsoleKey key;
+    }
+}
